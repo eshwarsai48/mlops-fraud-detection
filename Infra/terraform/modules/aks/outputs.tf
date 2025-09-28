46d378713244958c081eb3b4bc44b970f29ee182
eshwarsaivutla <eshwarsai48@gmail.com>
2025-09-28 01:01:52 -0400

    Networking Role Assignment to K8S

--- Infra/terraform/modules/aks/outputs.tf
+++ Infra/terraform/modules/aks/outputs.tf
@@ -2,3 +2,11 @@ output "kube_config" {
   value     = azurerm_kubernetes_cluster.rm_aks_dev.kube_config_raw
   sensitive = true
 }
+output "cluster_principal_id" {
+  value = azurerm_kubernetes_cluster.rm_aks_dev.identity[0].principal_id
+}
+
+# Kubelet identity – used for ACR pulls
+output "kubelet_object_id" {
+  value = azurerm_kubernetes_cluster.rm_aks_dev.kubelet_identity[0].object_id
+}
