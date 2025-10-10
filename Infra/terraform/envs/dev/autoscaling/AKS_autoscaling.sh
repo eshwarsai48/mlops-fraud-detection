7fabbde2d23ddcf73a80d28b0ac79bd9056f4123
eshwarsaivutla <eshwarsai48@gmail.com>
2025-10-10 14:47:54 -0400

    Autoscaling for AKS and Service monitoring setup for prometheus

---
+++ Infra/terraform/envs/dev/autoscaling/AKS_autoscaling.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ACTION=$1 # up or down
+RG="mlops-tfstate-rg"
+CLUSTER="fraud-aks"
+NODEPOOL="default"
+
+if [ "$ACTION" == "up" ]; then
+  echo "Scaling AKS up..."
+  az aks nodepool scale --resource-group $RG --cluster-name $CLUSTER --name $NODEPOOL --node-count 1
+elif [ "$ACTION" == "down" ]; then
+  echo "Scaling AKS down..."
+  az aks nodepool scale --resource-group $RG --cluster-name $CLUSTER --name $NODEPOOL --node-count 0
+else
+  echo "Usage: ./aks_scale.sh [up|down]"
+fi
